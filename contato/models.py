b763150bf8282058f84a3d584a53c3c58609fe8f
Alan Soares <a7soares@proton.me>
2024-06-13 22:38:39 -0300

    correção na configuração sistema de templates ok

--- contato/models.py
+++ contato/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+#from django.db import models
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+class Contato(models.Model):
+    nome = models.CharField('Nome',max_length=50)
+    sobrenome = models.CharField('Sobreno',max_length=50)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=15)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
